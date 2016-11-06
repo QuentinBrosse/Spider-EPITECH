c140e5385a55b35a0ddfcfd6c16c4199c1e39a23
Brendan Bobis <brendan.bobis@epitech.eu>
2016-11-06 12:19:23 +0100

    Protoco.hpp completed

--- Common/Protocol.hpp
+++ Common/Protocol.hpp
@@ -31,7 +31,18 @@ static std::map<unsigned char, std::string> m_keycode2name = {
 	{ 0x50, "P" },{ 0x51, "Q" },{ 0x52, "R" },
 	{ 0x53, "S" },{ 0x54, "T" },{ 0x55, "U" },
 	{ 0x56, "V" },{ 0x57, "W" },{ 0x58, "X" },
-	{ 0x59, "Y" },{ 0x5A, "Z" }
+	{ 0x59, "Y" },{ 0x5A, "Z" },{ 0x5B, "LEFT WINDOWS" },{ 0x5C, "RIGHT WINDOWS" },{ 0x5D, "APPLICATION" },
+	{ 0x5F, "SLEEP" },{ 0x60, "NUMPAD 0" },{ 0x61, "NUMPAD 1" },{ 0x62, "NUMPAD 2" },{ 0x63, "NUMPAD 3" },{ 0x64, "NUMPAD 4" },
+	{ 0x65, "NUMPAD 5" },{ 0x66, "NUMPAD 6" },{ 0x67, "NUMPAD 7" },{ 0x68, "NUMPAD 8" },{ 0x69, "NUMPAD 9" },{ 0x6A, "MULTIPLY" },
+	{ 0x6B, "ADD" },{ 0x6C, "SEPARATOR" },{ 0x6D, "SUBSTRACT" },{ 0x6E, "DECIMAL" },{ 0x6F, "DIVIDE" },{ 0x70, "F1" },
+	{ 0x71, "F2" },{ 0x72, "F3" },{ 0x73, "F4" },{ 0x74, "F5" },{ 0x75, "F6" },{ 0x76, "F7" },{ 0x77, "F8" },{ 0x78, "F9" },{ 0x79, "F10" },
+	{ 0x7A, "F11" },{ 0x7B, "F12" },{ 0x7C, "F13" },{ 0x7D, "F14" },{ 0x7E, "F15" },{ 0x7F, "F16" },{ 0x80, "F17" },{ 0x81, "F18" },{ 0x82, "F19" },
+	{ 0x83, "F20" },{ 0x84, "F21" },{ 0x85, "F22" },{ 0x86, "F23" },{ 0x87, "F24" },
+	{ 0x90, "NUM LOCK" },{ 0x91, "SCROLL LOCK" },{ 0x92, "SCROLL LOCK" },{ 0x93, "OEM SPECIFIC" },{ 0x94, "OEM SPECIFIC" },{ 0x95, "OEM SPECIFIC" },{ 0x96, "OEM SPECIFIC" },
+	{ 0xA0, "LEFT SHIFT" },{ 0xA1, "RIGHT SHIFT" },{ 0xA2, "LEFT CONTROL" },{ 0xA3, "RIGHT CONTROL" },{ 0xA4, "LEFT MENU" },{ 0xA5, "RIGHT MENU" },{ 0xA6, "BROWSER BACK" },{ 0xA7, "BROWSER FORWARD" },{ 0xA8, "BROWSER REFRESH" },{ 0xA9, "BROWSER STOP" },
+	{ 0xAA, "BROWSER SEARCH" },{ 0xAB, "BROWSER FAVORITES" },{ 0xAC, "BROWSER START AND HOME" },{ 0xAD, "VOLUME MUTE" },{ 0xAE, "VOLUME DOWN" },{ 0xAF, "VOLUME UP" },{ 0xB0, "NEXT TRACK" },
+	{ 0xB1, "PREVIOUS TRACK" },{ 0xB2, "STOP MEDIA" },{ 0xB3, "PLAY/PAUSE MEDIA" },{ 0xB4, "START MAIL" },{ 0xB5, "SELECT MEDIA" },{ 0xB6, "START APPLICATION 1" },{ 0xB7, "START APPLICATION 2" },
+	{ 0xE5, "IME PROCESS" },{ 0xF6, "ATTN" },{ 0xF7, "CRSEL" },{ 0xF8, "EXSEL" },{ 0xF9, "ERASE EOF" },{ 0xFA, "PLAY" },{ 0xFB, "ZOOM" },{ 0xFD, "PA1" },{ 0xFE, "CLEAR" }
 };
 
 static std::map<unsigned char, std::string> m_mousecode2name = {
